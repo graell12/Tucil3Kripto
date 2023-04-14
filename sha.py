379fa777d2c5f043e1e54fdf6e1d7cef911ee339
graell <gresyaleman12@gmail.com>
2023-04-14 21:10:34 +0700

    harusnya non-txt udah bisa

--- sha.py
+++ sha.py
@@ -1,9 +1,15 @@
 import hashlib
 import RSA
+from Crypto.Hash import SHA3_224
 
-def hash(text):
-    hash = hashlib.new("sha3_224", text.encode())
-    return hash.digest()
+def _hash(text, string=True):
+    if string:
+        return hashlib.new("sha3_224", text.encode()).digest()
+    return hashlib.new("sha3_224", text).digest()
 
+def hash(text, string=True):
+    if string:
+        return SHA3_224.new(text.encode()).digest()
+    return SHA3_224.new(text).digest()
 # def digitalSign(filename, priv_key):
     
--- utils.py
+++ utils.py
@@ -10,12 +10,13 @@ def sign(filename, priv):
 
     if tools.getextension(filename) == '.txt':
         contents = tools.readtxt(filename)
+        start_ds = contents.find("\n<ds>")
+        msgdigest = int.from_bytes(sha.hash(contents[:start_ds]), "big")
     else:
         contents = tools.readbin(filename)
+        msgdigest = int.from_bytes(sha.hash(contents, False), "big")
 
-    start_ds = contents.find("\n<ds>")
-    msgdigest = int.from_bytes(sha.hash(contents[:start_ds]), "big")
-    sign = RSA.dekripsiRSA(msgdigest, d, N)
+    sign = RSA.enkripsiRSA(msgdigest, d, N)
     digsign = f"""\n<ds>{sign}</ds>"""
 
     if tools.getextension(filename) == '.txt':
@@ -27,22 +28,22 @@ def sign(filename, priv):
 def validate(filename, publ, signfile=""):
     key = tools.readkeyfile(publ)
     N = int(key[1])
-    # print(N.bit_length())
     e = int(key[2])
+
     if tools.getextension(filename) == ".txt":
         contents = tools.readtxt(filename)
         pisah = re.split(r'\n<ds>|</ds>', contents)
         msgdigest = sha.hash(pisah[0])
-        # print(msgdigest)
+        sign = pisah[1]
     else:
         contents = tools.readbin(filename)
-        pisah = re.split(r'<ds>|</ds>', tools.readtxt(signfile))
-        msgdigest = sha.hash(pisah[0])
-    sign = pisah[1]
-    # print(sign)
-    signdig = RSA.enkripsiRSA(int(sign), e, N)
+        msgdigest = sha.hash(contents, False)
+        sign = tools.readtxt(signfile)
+        sign = re.split(r'\n<ds>|</ds>', sign)[1]
+        # sign = sign[sign.find('<ds>') + 4 : sign.find('</ds>')]
+
+    signdig = RSA.dekripsiRSA(int(sign), e, N)
     digest = int.from_bytes(msgdigest, "big")
-    # print("digest:", digest, "sign:", signdig)
     if digest == signdig:
         return("VALID.")
     else:
